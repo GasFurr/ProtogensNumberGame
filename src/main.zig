ff1b1c95608988e3aa081b617316a47ab81794b2
GasFurr <gasfurri@gmail.com>
2025-05-08 15:20:29 +0000

    Rename mainGameLoop to menuLoop

--- src/main.zig
+++ src/main.zig
@@ -1,148 +1,158 @@
 const std = @import("std");
 const utils = @import("utils.zig");
-// comptime T: type,b: []const T,a: []const T
+const print = std.debug.print;
+const strcompare = std.mem.eql;
 // Global allocator.
 pub const allocator = std.heap.page_allocator;
 
-//  Data:
+//  Difficulty data
 const Difficulty = enum {
     Easy,
     Medium,
     Hard,
     Chromium,
 };
+// Game states
 const GameState = enum {
     InMenu,
-    ScoreScreen,
+    ScoreScreen, // "scores" menu
     DifficultyScreen,
-    GameStarted,
+    GameStarted, // Pre-starting sequence
     WinScreen,
-    DebugMode,
-    unexpected,
+    DebugMode, // Debug menu
+    unexpected, // When something unexpected happens.
 };
-var debug: u32 = 0;
-var score: u32 = 0;
-var debug_score: u32 = 0;
-var max_score: []const u8 = "0";
-var current_state = GameState.InMenu;
-var current_difficulty = Difficulty.Easy;
-var main_loop: bool = true;
-var game_loop: bool = false;
-var new_max_score: bool = false;
+// Game data:
+var debug: u32 = 0; // Debug mode flag
+var score: u32 = 0; // Score counter
+var debug_score: u32 = 0; // Debug score counter
+var max_score: []const u8 = "0"; // Max score counter
+var current_state = GameState.InMenu; // Current game state
+var current_difficulty = Difficulty.Easy; // Current Difficulty
+var main_loop: bool = true; // Is main loop active?
+var game_loop: bool = false; // Is game loop active?
+var new_max_score: bool = false; // Is score > max_score?
 
-// In zig declaration order doesn't matter.
-// pub - makes function visible to other modules.
+// Main function
 pub fn main() !void {
     // Rendering welcome screen
     utils.clearTerminal();
     try utils.renderAscii(allocator, "resources/proto.txt");
     // Initialization stage
+    // Checking file integrity.
     try utils.initializeResources();
+    // Parsing max score.
     max_score = try utils.parseMaxScore(allocator);
-    defer allocator.free(max_score);
+    defer allocator.free(max_score); // Freeing memory.
 
-    try utils.waitForEnterKeyPress();
-    std.debug.print("Enter pressed", .{});
+    try utils.waitForEnterKeyPress(); // Self-explainatory fn
+    print("Enter pressed", .{}); //std.debug.print abbreviated
     // Calling main menu;
-    try mainGameLoop();
+    try MenuLoop();
 }
 
-fn mainGameLoop() !void {
+fn MenuLoop() !void {
+    // Is main loop active?
     while (main_loop == true) {
         switch (current_state) {
+            // Is GameState - in menu?
             GameState.InMenu => {
                 utils.clearTerminal();
                 new_max_score = false;
                 // Drawing main menu
                 try utils.renderAscii(allocator, "resources/menu.txt");
                 if (debug == 1) {
-                    std.debug.print("Debug mode is active, no scores!\n", .{});
-                    std.debug.print("Press 8 to reset all scores.", .{});
+                    print("Debug mode is active, no scores!\n", .{});
+                    print("Press 8 to reset all scores.", .{});
                 }
                 // Activating input
                 const input = try utils.readStdinLine(allocator);
                 defer allocator.free(input);
                 // Menu
 
-                if (std.mem.eql(u8, input, "0")) {
+                if (strcompare(u8, input, "0")) {
                     main_loop = false;
                     return;
-                } else if (std.mem.eql(u8, input, "1")) {
+                } else if (strcompare(u8, input, "1")) {
                     current_state = GameState.DifficultyScreen;
-                } else if (std.mem.eql(u8, input, "2")) {
+                } else if (strcompare(u8, input, "2")) {
                     current_state = GameState.ScoreScreen;
-                } else if (std.mem.eql(u8, input, "7")) {
+                } else if (strcompare(u8, input, "7")) {
                     current_state = GameState.DebugMode;
-                } else if (debug == 1 and std.mem.eql(u8, input, "8")) {
+                } else if (debug == 1 and strcompare(u8, input, "8")) {
                     try utils.resetMaxScore("resources/score");
                     try utils.resetMaxScore("resources/scoreDebug");
                 }
 
                 // -- End of Main Menu -- //
             },
+            // Is GameState - score screen?
             GameState.ScoreScreen => {
                 utils.clearTerminal();
                 if (score == 0 and debug == 0) {
-                    std.debug.print("Play a game first! \n", .{});
+                    print("Play a game first! \n", .{});
                 } else {
-                    std.debug.print("Your last steps count:{} \n", .{score});
+                    print("Your last steps count:{} \n", .{score});
                 }
                 const current_max = try utils.parseMaxScore(allocator);
                 defer allocator.free(current_max);
-                std.debug.print("Your least steps count: {s}\n", .{current_max});
-                std.debug.print("Press enter to go back...\n", .{});
+                print("Your least steps count: {s}\n", .{current_max});
+                print("Press enter to go back...\n", .{});
                 if (debug == 1) {
-                    std.debug.print("Debug score:", .{});
+                    print("Debug score:", .{});
                     try utils.renderAscii(allocator, "resources/scoreDebug");
                 }
                 try utils.waitForEnterKeyPress();
                 current_state = GameState.InMenu;
                 // -- End of Score Menu -- //
             },
+            // Is GameState - difficulty screen?
             GameState.DifficultyScreen => {
                 utils.clearTerminal();
                 try utils.renderAscii(allocator, "resources/startgame.txt");
                 const choice = try utils.readStdinLine(allocator);
                 defer allocator.free(choice);
-                if (std.mem.eql(u8, choice, "0")) {
+                if (strcompare(u8, choice, "0")) {
                     break;
-                } else if (std.mem.eql(u8, choice, "1")) {
+                } else if (strcompare(u8, choice, "1")) {
                     current_difficulty = Difficulty.Easy;
                     current_state = GameState.GameStarted;
-                } else if (std.mem.eql(u8, choice, "2")) {
+                } else if (strcompare(u8, choice, "2")) {
                     current_difficulty = Difficulty.Medium;
                     current_state = GameState.GameStarted;
-                } else if (std.mem.eql(u8, choice, "3")) {
+                } else if (strcompare(u8, choice, "3")) {
                     current_difficulty = Difficulty.Hard;
                     current_state = GameState.GameStarted;
-                } else if (std.mem.eql(u8, choice, "4")) {
+                } else if (strcompare(u8, choice, "4")) {
                     current_difficulty = Difficulty.Chromium;
                     current_state = GameState.GameStarted;
                 }
             },
+            // Is GameState - Game Started?
             GameState.GameStarted => {
                 utils.clearTerminal();
                 try GameStarted(current_difficulty);
             },
+            // Is GameState - Win Screen?
             GameState.WinScreen => {
                 // Drawing art + info + art in 3 stages
                 const current_max = try utils.parseMaxScore(allocator);
                 defer allocator.free(current_max);
                 try utils.renderAscii(allocator, "resources/winart.txt"); //image
-                std.debug.print("Your made {} guesses!\n", .{score});
-                std.debug.print("Least guesses {s}\n", .{current_max});
+                print("Your made {} guesses!\n", .{score});
+                print("Least guesses {s}\n", .{current_max});
                 try utils.renderAscii(allocator, "resources/winart2.txt"); //text
                 // Input + deallocation
                 const input = try utils.readStdinLine(allocator);
                 defer allocator.free(input);
                 // Menu
-                if (std.mem.eql(u8, input, "0")) {
+                if (strcompare(u8, input, "0")) {
                     break;
-                } else if (std.mem.eql(u8, input, "1")) {
+                } else if (strcompare(u8, input, "1")) {
                     current_state = GameState.DifficultyScreen;
-                } else if (std.mem.eql(u8, input, "2")) {
+                } else if (strcompare(u8, input, "2")) {
                     current_state = GameState.InMenu;
-                } else if (std.mem.eql(u8, input, "3")) {
+                } else if (strcompare(u8, input, "3")) {
                     try utils.resetMaxScore("resources/score");
                     if (debug == 1) {
                         try utils.resetMaxScore("resources/scoreDebug");
@@ -150,12 +160,13 @@ fn mainGameLoop() !void {
                 }
                 // -- End of WinScreen -- //
             },
+            // Is GameState - debug mode?
             GameState.DebugMode => {
-                std.debug.print("Do you want to enter debug mode?\n [Y/N]", .{});
+                print("Do you want to enter debug mode?\n [Y/N]", .{});
                 const input = try utils.readStdinLine(allocator);
                 defer allocator.free(input);
 
-                if (std.mem.eql(u8, input, "Y")) {
+                if (strcompare(u8, input, "Y")) {
                     debug = 1;
                     current_state = GameState.InMenu;
                 } else {
@@ -164,27 +175,31 @@ fn mainGameLoop() !void {
                 }
                 // -- End of Debug Mode Screen. -- //
             },
+            // In case of failure.
             GameState.unexpected => {
-                std.debug.print("If you are a cheater - why? \n If it's a bug.. Idk, restart the program", .{});
+                print("If you are a cheater - why? \n If it's a bug.. Idk, restart the program", .{});
                 try utils.waitForEnterKeyPress();
                 current_state = GameState.InMenu;
             },
         }
     }
 }
-
+// Real main game loop lol.
 pub fn GameStarted(difficulty: Difficulty) !void {
+    // Resetting score
     score = 0;
     debug_score = 0;
+    // Creating variable for number.
     var number: u32 = 0;
+    // always starts for now.
     if (true) {
         utils.clearTerminal();
         switch (difficulty) {
             Difficulty.Easy => {
-                std.debug.print("Wait a second, i am thinking...\n", .{});
+                print("Wait a second, i am thinking...\n", .{});
                 number = try utils.random(1, 101);
                 std.time.sleep(1254311000);
-                std.debug.print("Yeah, i am ready.\n", .{});
+                print("Yeah, i am ready.\n", .{});
                 game_loop = true;
             },
             Difficulty.Medium => {
@@ -201,13 +216,13 @@ pub fn GameStarted(difficulty: Difficulty) !void {
         }
 
         if (debug == 1) {
-            std.debug.print("DEBUG: The number is {}\n", .{number});
+            print("DEBUG: The number is {}\n", .{number});
         } else if (number == 0) {
             utils.clearTerminal();
             current_state = GameState.unexpected;
             return;
         } else {
-            std.debug.print("Let's start!\n", .{});
+            print("Let's start!\n", .{});
         }
         // Game loop.
         while (game_loop == true) {
@@ -215,13 +230,13 @@ pub fn GameStarted(difficulty: Difficulty) !void {
             if (debug == 1) {
                 debug_score += 1;
             }
-            std.debug.print("Your guess:", .{});
+            print("Your guess:", .{});
             const input = try utils.readStdinLine(allocator);
             defer allocator.free(input);
 
             const guess = std.fmt.parseInt(u32, input, 10) catch {
-                std.debug.print("\nIt's not a number! (at least it's hard for me to find it)", .{});
-                std.debug.print("\nI will not count that as guess. Your score is still {}\n", .{score});
+                print("\nIt's not a number! (at least it's hard for me to find it)", .{});
+                print("\nI will not count that as guess. Your score is still {}\n", .{score});
                 score -= 1; // Only count valid attempts
                 continue;
             };
@@ -229,11 +244,11 @@ pub fn GameStarted(difficulty: Difficulty) !void {
             if (guess == number) {
                 utils.clearTerminal();
                 if (score == 1) {
-                    std.debug.print("\nWow, first try!\n", .{});
+                    print("\nWow, first try!\n", .{});
                 } else {
-                    std.debug.print("\nYou win this time.\n", .{});
+                    print("\nYou win this time.\n", .{});
                 }
-                std.debug.print("\nThe number was {}\n", .{number});
+                print("\nThe number was {}\n", .{number});
 
                 // Update max score if needed
                 const current_max = try std.fmt.parseInt(u32, max_score, 10);
@@ -247,10 +262,10 @@ pub fn GameStarted(difficulty: Difficulty) !void {
                 main_loop = true;
                 return;
             } else if (guess > number) {
-                std.debug.print("\nI'm not eating that much! >w<\n", .{});
+                print("\nI'm not eating that much! >w<\n", .{});
                 continue;
             } else if (guess < number) {
-                std.debug.print("\nI would starve if it was true! owo\n", .{});
+                print("\nI would starve if it was true! owo\n", .{});
                 continue;
             }
         }
@@ -264,8 +279,8 @@ pub fn GameStarted(difficulty: Difficulty) !void {
 }
 
 pub fn ChromiumDifficulty() !void {
-    std.debug.print("In work...", .{});
-    std.debug.print("   o w o   ", .{});
+    print("In work...", .{});
+    print("   o w o   ", .{});
     try utils.waitForEnterKeyPress();
     current_state = GameState.InMenu;
 }
